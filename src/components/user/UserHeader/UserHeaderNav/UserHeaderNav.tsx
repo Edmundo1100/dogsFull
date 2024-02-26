95209fbc4e022839e5ad43e15f2e81d2681593dc
Edmundo dos Santos <37958683+Edmundo1100@users.noreply.github.com>
2024-02-26 14:29:04 -0300

    menu ok

--- src/components/user/UserHeader/UserHeaderNav/UserHeaderNav.tsx
+++ src/components/user/UserHeader/UserHeaderNav/UserHeaderNav.tsx
@@ -1,32 +1,53 @@
 import { NavLink } from "react-router-dom";
 import * as S from "./styles";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { userContext } from "../../../../context/userContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { useMedia } from "../../../../hooks/useMedia";
+import { useMedia } from "../../../../hooks/useMedia";
 
 export function UserHeaderNav() {
-  const {userLogout } = useContext(userContext);
-  // const mobile = useMedia('(max-with: 40rem)')
-  // console.log(mobile);
-  
+  const { userLogout } = useContext(userContext);
+  const mobile = useMedia("(max-width: 40rem)");
+  const [mobileMenu, setMobileMenu] = useState(false);
 
   return (
     <S.Container>
-      <NavLink to="/conta">
-        <FontAwesomeIcon icon={["far", "images"]} />
-      </NavLink>
-      <NavLink to="/conta/estatisticas">
-        <FontAwesomeIcon icon={["fas", "chart-line"]} />
-      </NavLink>
-      <NavLink to="/conta/postarFoto">
-        <FontAwesomeIcon icon={["fas", "plus"]} />
-      </NavLink>
+      <nav className={mobile ? "menuMobile" : "menuPC"}>
+        {mobile && (
+          <button
+            aria-label="Menu"
+            className="btnToggleMobile"
+            onClick={() => setMobileMenu(!mobileMenu)}
+          >
+            {mobileMenu && <FontAwesomeIcon icon={["fas", "bars"]} />}
+            {!mobileMenu && (
+              <FontAwesomeIcon icon={["fas", "ellipsis-vertical"]} />
+            )}
+          </button>
+        )}
 
-      <div className="btnSair" onClick={userLogout}>
-        <FontAwesomeIcon icon={["fas", "right-from-bracket"]} />
-      </div>
-      
+        {(!mobile || (mobile && !mobileMenu)) && (
+          <>
+            <NavLink to="/conta">
+              <FontAwesomeIcon icon={["far", "images"]} />
+              {mobile && "Conta"}
+            </NavLink>
+            <NavLink to="/conta/estatisticas">
+              <FontAwesomeIcon icon={["fas", "chart-line"]} />
+              {mobile && "Estastisticas"}
+            </NavLink>
+            <NavLink to="/conta/postarFoto">
+              <FontAwesomeIcon icon={["fas", "plus"]} />
+              {mobile && "Postar"}
+            </NavLink>
+
+            <div className="btnSair" onClick={userLogout}>
+              <FontAwesomeIcon icon={["fas", "right-from-bracket"]} />
+              {mobile && "Sair"}
+            </div>
+          </>
+        )}
+      </nav>
     </S.Container>
   );
 }
